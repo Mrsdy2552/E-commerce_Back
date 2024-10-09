0d4561618e8dd87717d867e41524b357ad25c17e
mrsdy <camelodiego1@hotmail.com>
2024-10-09 10:44:16 -0500

    frist Commit

---
+++ src/test/java/com/e_commerce_crud/e_commers/ECommersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce_crud.e_commers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
